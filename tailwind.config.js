c886dac1fb397389199a2988bc918e9e88f6b935
Olufemi Okikioluwa <101170232+samlufi@users.noreply.github.com>
2022-05-07 15:01:31 +0100

    Added colors safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['./*.html', './components/**/*.html', './admin/**/*.html'],
+  safelist: ['bg-red-500', 'bg-red-200'],
   theme: {
     extend: {
       fontFamily: {
